e0daa76ea8c0254537213260bf3aba10b461a84a
suisho <suisho000000@gmail.com>
2014-05-13 00:31:40 +0900

    import redcarpet

--- app/helpers/kss/application_helper.rb
+++ app/helpers/kss/application_helper.rb
@@ -5,7 +5,7 @@ module Kss
     # very easily.
     def styleguide_block(section, &block)
       raise ArgumentError, "Missing block" unless block_given?
-      
+
       @section = styleguide.section(section)
 
       if !@section.raw
@@ -15,5 +15,14 @@ module Kss
       content = capture(&block)
       render 'kss/shared/styleguide_block', :section => @section, :example_html => content
     end
+
+    def markdown(text)
+      @markdown ||= begin
+        renderer = Redcarpet::Render::HTML.new
+        @markdown = Redcarpet::Markdown.new(renderer)
+      end
+      @markdown.render(text).html_safe
+    end
+
   end
 end
